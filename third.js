bf1bdb2375d2811d4606e658b74b15f83204d313
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-06-29 13:06:32 +0530

    new push

---
+++ third.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    
+    },
+
+    autoplay: {
+      delay:111000,
+      // disableOnInteraction: true,
+    },
+    loop: true,
+  });
+
